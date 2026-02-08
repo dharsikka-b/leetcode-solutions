38060aff4b20aea8a0b7bebe753076f493344721
dharsikka-b <24149010@drngpit.ac.in>
2026-02-08 19:47:05 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,12 @@
+// Last updated: 08/02/2026, 19:47:04
+class Solution {
+    public boolean divisorGame(int n) {
+    
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
