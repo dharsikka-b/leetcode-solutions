ee90070df1f87f33dd0b39f677a4f1bbe3b33e62
dharsikka-b <24149010@drngpit.ac.in>
2026-02-08 19:47:25 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,13 @@
+// Last updated: 08/02/2026, 19:47:25
+class Solution {
+    public boolean canWinNim(int n) {
+
+        if(n%4==0){
+            return false;
+        }
+        else {
+            return true;
+        }  
+        
+    }
+}
