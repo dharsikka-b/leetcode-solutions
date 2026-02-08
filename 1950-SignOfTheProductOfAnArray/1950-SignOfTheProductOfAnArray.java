c3f24968fe953312668f7c071c571f05ceebc56d
dharsikka-b <24149010@drngpit.ac.in>
2026-02-08 19:46:50 +0530

    Create 1950-SignOfTheProductOfAnArray

---
+++ 1950-SignOfTheProductOfAnArray/1950-SignOfTheProductOfAnArray.java
@@ -0,0 +1,24 @@
+// Last updated: 08/02/2026, 19:46:49
+class Solution {
+    public int arraySign(int[] nums) {
+        int neg=0;
+        for(int x:nums){
+            if(x==0) return 0;
+            if(x<0) neg++;
+
+
+        }
+        return neg %2==0?1:-1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
